924bf11391fe9649691538c3db45eb89c270c712
Vips120 <vipulsingh0000@gmail.com>
2021-07-04 01:36:46 +0530

    ng pipe

---
+++ project4/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project4';
+  public username!: string;
+  public date!: string;
+  public currency!: string;
+  User(e: any) {
+    this.username = e.target.value;
+  };
+  onDateChange(e: any) {
+    this.date = e.target.value;
+  };
+  getCurrency(e: any) {
+    this.currency = e.target.value;
+  }
+}
